a5292561d00a7f322303f3f4b0d343e95d630176
Modulo <modzero@protonmail.com>
2020-06-17 11:29:37 -0500

    updated standard with recovery delay

--- standard.js
+++ standard.js
@@ -20,6 +20,7 @@ const config = {
   defaultRequestPriority: 200,
   discoveryRequestPriority: 100,
   bootstrapRequestPriority: 300,
+  recoveryDelay: 1000,
   dohEndpoints: [
     'https://cloudflare-dns.com/dns-query',
     'https://dns.google/resolve'
